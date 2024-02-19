d49b8b9fd181bf64a5f192c2d238d0ff3179c0a9
oNePlay <carnificem1993@gmail.com>
2024-02-19 20:22:36 +0300

    Исправленное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -33,11 +33,11 @@ public class Main {
         }
         int maxPay = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (minPay > arr[i]) {
-                maxPay=arr[i];
+            if (maxPay < arr[i]) {
+                maxPay = arr[i];
             }
         }
-       // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
         System.out.println("Минимальная сумма трат за день составляет " + minPay + " рублей." +
                 " \nМаксимальная сумма трат за день составила " + maxPay + " рублей.");
         //Задача 3
@@ -45,19 +45,19 @@ public class Main {
         int[] pay = generateRandomArray();
         float averageCons = 0;
         float summa = 0;
-            for (int i = 0; i < pay.length; i++) {
-                summa += pay[i];
-            }
-            averageCons = summa / pay.length;
-            System.out.println("Средняя сумма трат за месяц: " + averageCons + " рублей");
-            //Задача 4
-            System.out.println("Задача 4");
-            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-            for (int i = reverseFullName.length - 1; i >= 0; i--)
-                System.out.print(reverseFullName[i]);
-
+        for (int i = 0; i < pay.length; i++) {
+            summa += pay[i];
+        }
+        averageCons = summa / pay.length;
+        System.out.println("Средняя сумма трат за месяц: " + averageCons + " рублей");
+        //Задача 4
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
+}
 
 
 
